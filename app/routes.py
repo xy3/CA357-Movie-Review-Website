0d6d5c65776c63191dc5ebc63b9be66df0e7f41c
xy3 <a@xsq.pw>
2019-11-20 15:06:49 +0000

    New file structure

--- app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.route('/')
-def index():
-    user = {'username': 'Theo'}
-    return render_template('home.htm.j2', title='Movie Review Homepage', user=user)
