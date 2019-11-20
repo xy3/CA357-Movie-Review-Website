447d36e5c41467a2cba24219728e49883909c0db
xy3 <a@xsq.pw>
2019-11-20 15:32:06 +0000

    update

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 require_once 'vendor/autoload.php';
 require_once 'src/php/functions.inc.php';
 
+
+$base  = dirname($_SERVER['PHP_SELF']);
+
+// Update request when we have a subdirectory    
+if(ltrim($base, '/')){ 
+    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
+}
+
+// Dispatch as always
 $klein = new \Klein\Klein();
 
-$klein->respond('GET', '/', function () {
 
+$klein->respond('GET', '/', function () {
+	show('home.php');
 });
 
+
 $klein->dispatch();
+
--- src/php/functions.inc.php
+++ src/php/functions.inc.php
@@ -4,3 +4,9 @@ session_start();
 require_once 'dbc.inc.php';
 
 
+const VIEWS = 'src/views/';
+
+function show($page)
+{
+	require_once VIEWS . $page;
+}
